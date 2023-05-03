336470509e73892043522ef7eb34da6d00eae1e2
arslonga83 <jlwood83@gmail.com>
2023-05-03 09:22:47 -0700

    add tls support to email

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false, // upgrade later with STARTTLS
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
